c1e2cb374e1d6d9077eba9fef69f882043bf7df6
AnyaKhabarova <76184706+AnyaKhabarova@users.noreply.github.com>
2021-03-03 09:58:06 -0800

    Ex.8.1.4

---
+++ FileSystem_Class_Ex.8.1.4/FileSystem_Class_Ex.8.1.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FileSystem_Class_Ex._8._1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class FileSystemInfo
+    {
+        public FileSystemInfo(string name, long totalSpace, long freeSpace)
+        {
+            DiskName = name;
+            DiskVolume = totalSpace;
+            FreeSpace = freeSpace;
+        }
+
+        public string DiskName { get; }
+        public long DiskVolume { get; }
+        public long FreeSpace { get; }
+
+    }
+}
