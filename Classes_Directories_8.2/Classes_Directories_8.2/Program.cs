eb96f74fedb77d6d0f37bca14e9922eef9fef79a
AnyaKhabarova <76184706+AnyaKhabarova@users.noreply.github.com>
2021-03-03 20:37:51 -0800

    DriveInfo

---
+++ Classes_Directories_8.2/Classes_Directories_8.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Classes_Directories_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Name: {drive.Name}");
+                Console.WriteLine($"Type: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Size: {drive.TotalSize}");
+                    Console.WriteLine($"Space: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Tag: {drive.VolumeLabel}");
+                }
+            }
+        }
+    }
+}
