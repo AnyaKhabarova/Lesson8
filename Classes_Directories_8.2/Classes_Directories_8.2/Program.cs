f12fee018de6b063a540f05eb989f65dad8bea3a
AnyaKhabarova <76184706+AnyaKhabarova@users.noreply.github.com>
2021-03-03 21:02:17 -0800

    FilesCounter Ex.8.2.1

--- Classes_Directories_8.2/Classes_Directories_8.2/Program.cs
+++ Classes_Directories_8.2/Classes_Directories_8.2/Program.cs
@@ -7,40 +7,55 @@ namespace Classes_Directories_8._2
     {
         static void Main(string[] args)
         {
-            
-/*            DriveInfo[] drives = DriveInfo.GetDrives();
 
-            foreach (DriveInfo drive in drives)
+            /*            DriveInfo[] drives = DriveInfo.GetDrives();
+
+                        foreach (DriveInfo drive in drives)
+                        {
+                            Console.WriteLine($"Name: {drive.Name}");
+                            Console.WriteLine($"Type: {drive.DriveType}");
+                            if (drive.IsReady)
+                            {
+                                Console.WriteLine($"Size: {drive.TotalSize}");
+                                Console.WriteLine($"Space: {drive.TotalFreeSpace}");
+                                Console.WriteLine($"Tag: {drive.VolumeLabel}");
+                            }
+                        }*/
+
+            /* GetCatalogs();
+
+             static void GetCatalogs()
+             {
+                 string dirName = @"E:\\";
+                 if (Directory.Exists(dirName))
+                 {
+                     Console.WriteLine("Folders:");
+                     string[] dirs = Directory.GetDirectories(dirName);
+
+                     foreach (string d in dirs)                    
+                         Console.WriteLine(d);
+
+                     Console.WriteLine();
+                     Console.WriteLine("Files:");
+                     string[] files = Directory.GetFiles(dirName);
+
+                     foreach (string f in files)
+                         Console.WriteLine(f);
+                 }
+             }*/
+
+            FilesCounter();
+
+            static void FilesCounter()
             {
-                Console.WriteLine($"Name: {drive.Name}");
-                Console.WriteLine($"Type: {drive.DriveType}");
-                if (drive.IsReady)
-                {
-                    Console.WriteLine($"Size: {drive.TotalSize}");
-                    Console.WriteLine($"Space: {drive.TotalFreeSpace}");
-                    Console.WriteLine($"Tag: {drive.VolumeLabel}");
-                }
-            }*/
-
-            GetCatalogs();
-
-            static void GetCatalogs()
-            {
-                string dirName = @"E:\\";
+                string dirName = @"C:\\";
                 if (Directory.Exists(dirName))
                 {
-                    Console.WriteLine("Folders:");
-                    string[] dirs = Directory.GetDirectories(dirName);
-
-                    foreach (string d in dirs)                    
-                        Console.WriteLine(d);
-
-                    Console.WriteLine();
-                    Console.WriteLine("Files:");
                     string[] files = Directory.GetFiles(dirName);
+                    Console.WriteLine(files.Length+1);
 
-                    foreach (string f in files)
-                        Console.WriteLine(f);
+                    string[] folders = Directory.GetDirectories(dirName);
+                    Console.WriteLine(folders.Length+1);
                 }
             }
         }
